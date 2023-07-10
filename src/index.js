1b80ae5d7f49cf00b8b65b7987563c5a4504ca76
Aadityajoshi151 <aadityajoshi151@gmail.com>
2023-07-10 16:28:14 +0530

    Add option to search by pressing enter key

--- src/index.js
+++ src/index.js
@@ -4,27 +4,35 @@ const input_bar = document.getElementById("inputbar");
 
 websites = [];
 
-search_button.addEventListener("click", function() {
-    var new_window = document.getElementById("new_window_check")
-    if(new_window.checked){
-    chrome.windows.create({ focused: true }, function(window) {
-        var initialTabCreated = false;
-        websites.forEach(site => {
-            //Create the url by using input_bar.value here
-            if (!initialTabCreated) {
-                chrome.tabs.update(window.tabs[0].id, { url: site });
-                initialTabCreated = true;
-              } else {
-                chrome.tabs.create({ url: site, windowId: window.id });
-              }
-        })
-      });
+input_bar.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+      search_button.click();
+    }})
+
+search_button.addEventListener("click", function () {
+    var query = input_bar.value;
+    alert(query)
+    var new_window = document.getElementById("new_window_check");
+    if (new_window.checked) {
+        chrome.windows.create({ focused: true }, function (window) {
+            var initialTabCreated = false;
+            websites.forEach(site => {
+                //Create the url by using input_bar.value here
+                if (!initialTabCreated) {
+                    chrome.tabs.update(window.tabs[0].id, { url: site });
+                    initialTabCreated = true;
+                } else {
+                    chrome.tabs.create({ url: site, windowId: window.id });
+                }
+            })
+        });
     }
-    else{
+    else {
         websites.forEach(site => {
             //Create the url by using input_bar.value here
-                chrome.tabs.create({ url: site});
-    })
-}
+            chrome.tabs.create({ url: site });
+        })
+    }
 
 })
