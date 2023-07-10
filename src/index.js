49700aae297c1f55cad8e82f8e5095e8b3b174f0
Aadityajoshi151 <aadityajoshi151@gmail.com>
2023-07-10 14:06:35 +0530

    Add logic to open in new window

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const search_button = document.getElementById("searchbtn");
 const input_bar = document.getElementById("inputbar");
 
-websites = []
+websites = [];
 
 search_button.addEventListener("click", function() {
-    websites.forEach(site => {
-        //Create the url by using input_bar.value here
-        chrome.tabs.create({ url: site}); 
-    });
+    chrome.windows.create({ focused: true }, function(window) {
+        var initialTabCreated = false;
+        websites.forEach(site => {
+            //Create the url by using input_bar.value here
+            if (!initialTabCreated) {
+                chrome.tabs.update(window.tabs[0].id, { url: site });
+                initialTabCreated = true;
+              } else {
+                chrome.tabs.create({ url: site, windowId: window.id });
+              }
+        })
+      });
 })
