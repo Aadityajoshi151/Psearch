9568ddbfb8712babe29b9df6254fe6de2d4e801c
Aadityajoshi151 <aadityajoshi151@gmail.com>
2023-07-10 14:17:56 +0530

    Add option to toggle new window

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const search_button = document.getElementById("searchbtn");
 const input_bar = document.getElementById("inputbar");
 
+
 websites = [];
 
 search_button.addEventListener("click", function() {
+    var new_window = document.getElementById("new_window_check")
+    if(new_window.checked){
     chrome.windows.create({ focused: true }, function(window) {
         var initialTabCreated = false;
         websites.forEach(site => {
@@ -16,4 +19,12 @@ search_button.addEventListener("click", function() {
               }
         })
       });
+    }
+    else{
+        websites.forEach(site => {
+            //Create the url by using input_bar.value here
+                chrome.tabs.create({ url: site});
+    })
+}
+
 })
